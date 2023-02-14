5d1da11bbec81855b6236d71d446a1fd7b8ee46f
JimJ92120 <57893611+JimJ92120@users.noreply.github.com>
2023-02-14 18:16:42 +0700

    refactor(options media): add App and refactor ImageUpload

---
+++ src/admin/options-media/App.js
@@ -0,0 +1,30 @@
+import { useState } from "@wordpress/element";
+import { useSelect } from "@wordpress/data";
+import { store } from "@wordpress/core-data";
+
+import ImageUpload from "./components/ImageUpload";
+
+const OPTION_KEY = "image_watermark_settings";
+
+export default function App() {
+  const [settings, setSettings] = useState(null);
+
+  useSelect((select) => {
+    const result = select("core").getEntityRecord("root", "site");
+
+    if (!settings && result && result[OPTION_KEY]) {
+      setSettings(result[OPTION_KEY]);
+    }
+  });
+
+  return (
+    <div>
+      {settings && settings.image_id && (
+        <ImageUpload
+          imageId={settings.image_id}
+          fieldKey={OPTION_KEY + "[image_id]"}
+        />
+      )}
+    </div>
+  );
+}
