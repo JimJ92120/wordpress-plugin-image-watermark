a193d31314aa386848b25eccf8d558ce02e4254d
JimJ92120 <57893611+JimJ92120@users.noreply.github.com>
2023-02-15 15:15:30 +0700

    Revert "Release/0.1.0"

--- src/admin/options-media/index.js
+++
@@ -1,9 +0,0 @@
-import { render } from "@wordpress/element";
-
-import App from "./App";
-
-addEventListener("DOMContentLoaded", () => {
-  const $fieldContainer = document.getElementById("image-watermark");
-
-  render(<App />, $fieldContainer);
-});
