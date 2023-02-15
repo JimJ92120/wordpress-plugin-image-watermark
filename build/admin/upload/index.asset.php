0aa147611233d2a8d0f0a548111d60cd627ac53d
JimJ92120 <57893611+JimJ92120@users.noreply.github.com>
2023-02-15 18:26:58 +0700

    chore(build): add 0.1.1 build and vendor

---
+++ build/admin/upload/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '166e6ffe205248296741');
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'jim/wordpress-plugin-image-watermark',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '437d3012cf3f80271a63392a10f0fbcc4531a09e',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'jim/wordpress-plugin-image-watermark' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '437d3012cf3f80271a63392a10f0fbcc4531a09e',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
