cc3401ed13c455855b5af52b391945c322f4d9e7
JimJ921120 <57893611+JimJ92120@users.noreply.github.com>
2023-02-16 20:21:24 +0700

    refactor(upload > views): extract views extending from wp views

--- src/admin/upload/index.js
+++ src/admin/upload/index.js
@@ -1,48 +1,5 @@
-import WatermarkSettingsView from "./components/WatermarkSettingsView";
-import AddWatermarkButtonView from "./components/AddWatermarkButtonView";
+import ToolbarView from "./views/ToolbarView";
+import TwoColumnView from "./views/TwoColumnView";
 
-const { TwoColumn } = wp.media.view.Attachment.Details;
-wp.media.view.Attachment.Details.TwoColumn = TwoColumn.extend({
-  initialize() {
-    TwoColumn.prototype.initialize.apply(this, arguments);
-
-    const { attributes: image } = this.model;
-    this._watermarkSettingsView = new WatermarkSettingsView({
-      image: {
-        url: image.originalImageURL ?? image.url,
-        height: image.height,
-        width: image.width,
-        title: image.title,
-      },
-    });
-
-    return this;
-  },
-
-  render() {
-    TwoColumn.prototype.render.apply(this, arguments);
-
-    this.el.querySelector(".settings").prepend(this._watermarkSettingsView.el);
-
-    return this;
-  },
-});
-
-const { Toolbar } = wp.media.view;
-wp.media.view.Toolbar = Toolbar.extend({
-  _watermarkButtonView: new AddWatermarkButtonView(),
-  _deleteButtonClassName: "delete-selected-button",
-
-  initialize() {
-    Toolbar.prototype.initialize.apply(this, arguments);
-
-    this.controller.on("select:activate", () => {
-      this.el
-        .querySelector(`.${this._deleteButtonClassName}`)
-        .before(this._watermarkButtonView.el);
-    });
-    this.controller.on("select:deactivate", () => {
-      this._watermarkButtonView.el.remove();
-    });
-  },
-});
+wp.media.view.Attachment.Details.TwoColumn = TwoColumnView;
+wp.media.view.Toolbar = ToolbarView;
