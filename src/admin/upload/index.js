b5ff39cbdd243a4b2c979aed185ee58382fb3abb
JimJ921120 <57893611+JimJ92120@users.noreply.github.com>
2023-02-17 00:20:42 +0700

    refactor(upload > views > two column): return a function

--- src/admin/upload/index.js
+++ src/admin/upload/index.js
@@ -1,5 +1,5 @@
 import ToolbarView from "./views/ToolbarView";
 import TwoColumnView from "./views/TwoColumnView";
 
-wp.media.view.Attachment.Details.TwoColumn = TwoColumnView;
+wp.media.view.Attachment.Details.TwoColumn = TwoColumnView();
 wp.media.view.Toolbar = ToolbarView;
--- src/admin/upload/views/TwoColumnView/index.js
+++ src/admin/upload/views/TwoColumnView/index.js
@@ -3,30 +3,33 @@ import WatermarkSettingsView from "./WatermarkSettingsView";
 // https://atimmer.github.io/wordpress-jsdoc/media_views_attachment_details-two-column.js.html
 const { TwoColumn } = wp.media.view.Attachment.Details;
 
-const TwoColumnView = TwoColumn.extend({
-  initialize() {
-    TwoColumn.prototype.initialize.apply(this, arguments);
+const TwoColumnView = () =>
+  TwoColumn.extend({
+    initialize() {
+      TwoColumn.prototype.initialize.apply(this, arguments);
 
-    const { attributes: image } = this.model;
-    this._watermarkSettingsView = new WatermarkSettingsView({
-      image: {
-        url: image.originalImageURL ?? image.url,
-        height: image.height,
-        width: image.width,
-        title: image.title,
-      },
-    });
+      const { attributes: image } = this.model;
+      this._watermarkSettingsView = new WatermarkSettingsView({
+        image: {
+          url: image.originalImageURL ?? image.url,
+          height: image.height,
+          width: image.width,
+          title: image.title,
+        },
+      });
 
-    return this;
-  },
+      return this;
+    },
 
-  render() {
-    TwoColumn.prototype.render.apply(this, arguments);
+    render() {
+      TwoColumn.prototype.render.apply(this, arguments);
 
-    this.el.querySelector(".settings").prepend(this._watermarkSettingsView.el);
+      this.el
+        .querySelector(".settings")
+        .prepend(this._watermarkSettingsView.el);
 
-    return this;
-  },
-});
+      return this;
+    },
+  });
 
 export default TwoColumnView;
