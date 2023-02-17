bf0dc984aee8e3cb567ca281558654e3c37dbe69
JimJ921120 <57893611+JimJ92120@users.noreply.github.com>
2023-02-17 23:55:41 +0700

    refactor(upload > components > watermark button) separate view and controller / events

--- src/admin/upload/components/AddWatermarkButton/index.js
+++ src/admin/upload/components/AddWatermarkButton/index.js
@@ -1,95 +1,82 @@
+import WatermarkButtonView from "./WatermarkButtonView";
+
 import { generateAndSaveMarkedImage } from "./watermark";
 
-const AddWatermarkButton = _.extend(
-  Backbone.View.extend({
-    tagName: "button",
-    className: "image-watermark media-button button button-large",
-    template: "Add watermark",
-    events: {
+const { Events } = Backbone;
+class WatermarkButton extends _.extend(WatermarkButtonView, Events) {
+  get events() {
+    return {
       click: "click",
-    },
+    };
+  }
 
-    isSingle: true,
+  isSingle = true;
 
-    initialize(props = {}) {
-      if (props) {
-        if (props.selection) {
-          this.selection = props.selection;
-        }
+  constructor(props) {
+    super(props);
 
-        if (props.isSingle || props.isSingle === false) {
-          this.isSingle = props.isSingle;
-        }
+    if (props) {
+      if (props.selection) {
+        this.selection = props.selection;
       }
 
-      this.bind("loadingStart", null, this);
-      this.bind("loadingEnd", null, this);
-      this.bind("saveResult", null, this);
-
-      this.render();
-    },
-
-    render() {
-      this.$el.html(this.template);
-
-      return this;
-    },
-
-    setImage(image) {
-      this.image = image;
-    },
-
-    async click() {
-      console.log("clicked");
-
-      if (this.selection) {
-        console.log("start");
-        this.trigger("loadingStart");
-
-        const result = await (this.isSingle
-          ? this._generateSingleImage()
-          : this._generateMultipleImages());
-
-        this.trigger("saveResult", result);
-        this.trigger("loadingEnd");
-
-        console.log("end", result);
+      if (props.isSingle || props.isSingle === false) {
+        this.isSingle = props.isSingle;
       }
-    },
+    }
 
-    async _generateSingleImage() {
-      const { attributes: image } = this.selection;
+    this.bind("loadingStart", null, this);
+    this.bind("loadingEnd", null, this);
+    this.bind("saveResult", null, this);
+  }
 
-      return generateAndSaveMarkedImage(
+  async click() {
+    console.log("clicked");
+
+    if (this.selection) {
+      console.log("start");
+      this.trigger("loadingStart");
+
+      const result = await (this.isSingle
+        ? this._generateSingleImage()
+        : this._generateMultipleImages());
+
+      this.trigger("saveResult", result);
+      this.trigger("loadingEnd");
+
+      console.log("end", result);
+    }
+  }
+
+  async _generateSingleImage() {
+    const { attributes: image } = this.selection;
+
+    return generateAndSaveMarkedImage(
+      {
+        url: image.url,
+        height: image.height,
+        width: image.width,
+        title: image.title,
+      },
+      "png"
+    );
+  }
+
+  async _generateMultipleImages() {
+    const promises = this.selection.models.map(({ attributes }) =>
+      generateAndSaveMarkedImage(
         {
-          url: image.url,
-          height: image.height,
-          width: image.width,
-          title: image.title,
+          url: attributes.url,
+          title: attributes.title,
+          height: attributes.height,
+          width: attributes.width,
         },
         "png"
-      );
-    },
+      )
+    );
 
-    async _generateMultipleImages() {
-      const promises = this.selection.models.map(({ attributes }) =>
-        generateAndSaveMarkedImage(
-          {
-            url: attributes.url,
-            title: attributes.title,
-            height: attributes.height,
-            width: attributes.width,
-          },
-          "png"
-        )
-      );
+    return Promise.all(promises, (result) => result);
+  }
+}
 
-      return Promise.all(promises, (result) => result);
-    },
-  }),
-  Backbone.Events
-);
-
-export default AddWatermarkButton;
-
-export { generateAndSaveMarkedImage };
+export default WatermarkButton;
