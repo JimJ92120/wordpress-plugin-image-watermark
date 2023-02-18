789f8cd3d09e2107cc89ffae6a019343ff58746b
JimJ921120 <57893611+JimJ92120@users.noreply.github.com>
2023-02-18 12:27:45 +0700

    refactor(upload > watermark > api): replace api call with @wordpress/fetch-api for saveImage(

--- src/admin/upload/watermark/api.js
+++ src/admin/upload/watermark/api.js
@@ -46,26 +46,15 @@ const fetchWatermarkImage = async () => {
   return false;
 };
 
-// replace with wp.api.models.Media.save()
-const saveImage = (imageBlob, imageName, extension) => {
-  // return false;
-  return fetch(wp.api.utils.getRootUrl() + "/wp-json/wp/v2/media", {
+const saveImage = async (imageBlob, imageName, extension) => {
+  return apiFetch({
+    path: "wp/v2/media",
     method: "POST",
     headers: {
-      "X-WP-Nonce": wpApiSettings.nonce,
       "Content-Disposition": `attachment; filename="${imageName}.${extension}"`,
-      "Content-type": `image/${extension}`,
     },
     body: imageBlob,
-  })
-    .then((response) => {
-      if (201 === response.status) {
-        return response.json();
-      }
-
-      return false;
-    })
-    .catch((error) => console.error(error));
+  }).catch(() => false);
 };
 
 export { fetchWatermarkImage, saveImage };
